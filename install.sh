d7f3b44218ae795b4d93a7713809b86b8141e609
Sam Gamble <hexadessa@gmail.com>
2019-04-08 16:45:59 +0100

    Update install.sh - git clones

--- install.sh
+++ install.sh
@@ -11,36 +11,39 @@ GIT_API=OasisPlatform
 GIT_PIWIND=OasisPiWind
 
 
-# Clone and update repos 
-cd $SCRIPT_DIR
+# ---  OASIS UI --- # 
 if [ -d $SCRIPT_DIR/$GIT_UI ]; then
     cd $SCRIPT_DIR/$GIT_UI
     git fetch && git checkout $VERS_UI
 else
-    # Git Clone UI
-    git clone https://github.com/OasisLMF/$GIT_UI.git 
-    git checkout -b $VERS_UI
+    mkdir -p $SCRIPT_DIR/$GIT_UI
+    cd $SCRIPT_DIR/$GIT_UI
+    git clone https://github.com/OasisLMF/$GIT_UI.git .
+    git checkout $VERS_UI
 fi 
 
-cd $SCRIPT_DIR
+# ---  OASIS API --- # 
 if [ -d $SCRIPT_DIR/$GIT_API ]; then
     cd $SCRIPT_DIR/$GIT_API
     git fetch && git checkout $VERS_API
 else
-    # Git Clone API
-    git clone https://github.com/OasisLMF/$GIT_API.git 
-    git checkout -b $VERS_API
+    mkdir -p $SCRIPT_DIR/$GIT_API
+    cd $SCRIPT_DIR/$GIT_API
+    git clone https://github.com/OasisLMF/$GIT_API.git .
+    git checkout $VERS_API
 fi 
 
-cd $SCRIPT_DIR
+# ---  MODEL PiWind --- # 
 if [ -d $SCRIPT_DIR/$GIT_PIWIND ]; then
     cd $SCRIPT_DIR/$GIT_PIWIND
     git fetch && git checkout $VERS_PIWIND
 else
-    # Git Clone PiWind
-    git clone https://github.com/OasisLMF/$GIT_PIWIND.git 
-    git checkout -b $VERS_PIWIND
+    mkdir -p $SCRIPT_DIR/$GIT_PIWIND
+    cd $SCRIPT_DIR/$GIT_PIWIND
+    git clone https://github.com/OasisLMF/$GIT_PIWIND.git .
+    git checkout $VERS_PIWIND
 fi 
+
 
 
 # setup and run API
