0e1e151d45c7dbb5c7b2253e752bb8ff3418e433
Sam Gamble <hexadessa@gmail.com>
2026-06-23 09:36:01 +0100

    Add MySQL example

--- install.sh
+++ install.sh
@@ -68,5 +68,5 @@ docker pull coreoasis/oasisui_app:$VERS_UI
 set -e
 
 # RUN OasisPlatform / OasisUI / Portainer
-docker compose -f $SCRIPT_DIR/oasis-platform.yml -f $SCRIPT_DIR/oasis-ui-standalone.yml up -d --no-build
+docker compose -f $SCRIPT_DIR/oasis-platform-mysql.yml -f $SCRIPT_DIR/oasis-ui-standalone.yml up -d --no-build
 docker compose -f $SCRIPT_DIR/portainer.yaml up -d
